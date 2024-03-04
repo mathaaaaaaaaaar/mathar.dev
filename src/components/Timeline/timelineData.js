8710c31a1a0e1a8184168e74701b45eeed29873f
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2024-03-04 02:24:09 -0500

    minor updates

---
+++ src/components/Timeline/timelineData.js
@@ -0,0 +1,56 @@
+const timelineData = [
+    {
+        role: 'College Graduate Certificate - Mobile Solutions Development',
+        company: 'Conestoga College',
+        duration: 'September 2023 - December 2024',
+        points: [
+            ''
+        ],
+        workOrEd: 'ed',
+    },
+    {
+        role: 'Customer Success Engineer',
+        company: 'WeCP - We Create Problems',
+        duration: 'January 2023 - June 2023',
+        points: [
+            'Worked closely with 20+ enterprise-level customers, helping them hire qualified technical talent, more efficiently',
+            'Built the Customer Success function from scratch, by hiring 3 new Success Engineers, creating customer support systems, and setting up team processes',
+            'Assisted the Product Team, with inputs from Sales, Success and Support on Customer needs and wants'
+        ],
+        wkOrEd: 'wk',
+    },
+    {
+        role: 'Software Development Engineer',
+        company: 'Speechify',
+        duration: 'August 2022 - November 2022',
+        points: [
+            'Worked on  a Text-To-Speech Web App, Chrome, and Safari extension, helping 1,000,000+ users read with ease',
+            'Reduced uninstalls from 15% to 6%, by identifying and fixing broken experiences for customers',
+            'Consistently listened to User feedback and built new features, further improving user retention'
+        ],
+        wkOrEd: 'wk',
+    },
+    {
+        role: 'Technical Support Engineer',
+        company: 'Swym',
+        duration: 'May 2021 - August 2022',
+        points: [
+            'Part of a dynamic, customer-facing team, at a growing SaaS start-up with a worldwide customer base of 30,000+ e-commerce stores',
+            'Built an engineering team, to handle custom solutions for Enterprise Shopify Stores, generating ARR for Swym upwards of $115,000',
+            'Responsible for creating unique solutions for 85+ e-commerce stores weekly',
+            'Collaborated with 3+ teams to create internal solutions and tools for increased productivity and efficiency'
+        ],
+        wkOrEd: 'wk',
+    },
+    {
+        role: 'Bachelor of Technology - Information Technology (IT)',
+        company: 'College of Engineering, Guindy - Anna University',
+        duration: 'September 2017 - June 2021',
+        points: [
+            ''
+        ],
+        wkOrEd: 'ed',
+    },
+];
+
+export default timelineData;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HeroSection from '../components/HeroSection';
+import TimelineComponent from '../components/Timeline';
+
+const Home = () => {
+  return (
+    <div >
+        <HeroSection />
+        <TimelineComponent />
+    </div>
+  );
+};
+  
+export default Home;
