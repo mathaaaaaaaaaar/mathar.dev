a2ea3470b5ca6c24f4781e990a384d0b6209fccb
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2024-06-30 16:30:17 -0400

    added vercel config

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -66,26 +66,6 @@ const TimelineComponent = () => {
     }, []);
 
     return (
-        // <div className='timeline-parent'>
-        //     <Divider />
-        //     <Timeline className="timeline-main" position="alternate">
-        //         {timelineData.map((event, index) => (
-        //             <TimelineItem key={index}>
-        //                 <TimelineOppositeContent className={`${selectedDot === index ? 'opposite-fade-in' : 'opposite-fade-out'} ${index % 2 === 0 ? '' : 'alternate'}`} sx={{ m: 'auto 0' }} align="right" variant="body2">{selectedDot === index && (event.wkOrEd === "wk"? "Work Experience" : "Education")}</TimelineOppositeContent>
-        //                 <TimelineSeparator>
-        //                     <TimelineConnector />
-        //                     <TimelineDot className={`timeline-dot ${event.wkOrEd === "wk" ? 'work-timeline-dot' : 'school-timeline-dot'} ${selectedDot === index ? 'timeline-dot-active' : ''}`} onClick={() => handleDotClick(index)}>
-        //                         {event.wkOrEd === "wk"? <Work /> : <School />}
-        //                     </TimelineDot>
-        //                     <TimelineConnector />
-        //                 </TimelineSeparator>
-        //                 <TimelineContent className={`${selectedDot === index ? 'fade-in' : 'fade-out'} ${index % 2 === 0 ? '' : 'alternate'}`} sx={{ py: '12px', px: 2 }}>
-        //                 {selectedDot === index && <TimelineInfo event={event} />}
-        //                 </TimelineContent>
-        //             </TimelineItem>
-        //         ))}
-        //     </Timeline>
-        // </div>
 
         <div className='timeline-parent'>
             <Divider />
